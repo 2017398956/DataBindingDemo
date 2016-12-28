f0605a4cbac2c2933ca0eac27850bfbf5dc96c06
choochoo <2017398956@qq.com>
2016-12-28 09:46:42 +0800

    alert .gitignore

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="7bab99dd-647e-4d60-a76e-8769967aeba1", modulePackage="com.connorlin.databinding", sdkRoot="D:\\nfl\\Android\\sdk",layoutInfoDir="D:\\nfl\\Android\\android-studio-workplace\\DataBindingDemo-master\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=17,enableDebugLogs=false,printEncodedError=true)
-public class DataBindingInfo {}
